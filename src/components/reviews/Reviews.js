f144ddbc463c63b96c074015a23392edeca1dada
fentybit <fentymhall@gmail.com>
2021-04-14 21:19:09 -0700

    final submit

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,15 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const filteredReviews = reviews.filter( review => review.restaurantId === restaurantId )
+
+    const displayReviews = filteredReviews.map( review => <Review key={review.id} review={review} deleteReview={deleteReview} /> )
+
     return (
       <ul>
         Reviews
+        { displayReviews }
       </ul>
     );
   }
